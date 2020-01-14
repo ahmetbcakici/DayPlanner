2206f619297a60a193714bcb21ec81c998b25a29
ahmetbcakici <ahmetbcakici@gmail.com>
2020-01-14 21:55:09 +0300

    developing

--- client/src/components/ErrorAlert.js
+++ client/src/components/ErrorAlert.js
@@ -9,7 +9,6 @@ export default class ErrorAlert extends Component {
 		return (
 			<div class="alert alert-danger mt-3 mb-0" role="alert">
 				{this.props.msg}
-                xy
 			</div>
 		);
 	}
