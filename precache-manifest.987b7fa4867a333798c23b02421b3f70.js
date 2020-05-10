6fc82805deefc073024803a2a27506a697829a6a
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-11 01:23:27 +0300

    Updates

---
+++ precache-manifest.987b7fa4867a333798c23b02421b3f70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cf847f5cec6e3ff3af3d358012ebd1",
+    "url": "/dayplanner/index.html"
+  },
+  {
+    "revision": "f9a17851f3e0234d99a7",
+    "url": "/dayplanner/static/css/2.7e9c9ab4.chunk.css"
+  },
+  {
+    "revision": "f99c079f8e478ad973a4",
+    "url": "/dayplanner/static/css/main.f6676d6d.chunk.css"
+  },
+  {
+    "revision": "f9a17851f3e0234d99a7",
+    "url": "/dayplanner/static/js/2.752d977e.chunk.js"
+  },
+  {
+    "revision": "69e94bbe030b9f4b08985474d85202c2",
+    "url": "/dayplanner/static/js/2.752d977e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f99c079f8e478ad973a4",
+    "url": "/dayplanner/static/js/main.4e8e8091.chunk.js"
+  },
+  {
+    "revision": "08df441f0fbc0a5f0cb6",
+    "url": "/dayplanner/static/js/runtime-main.5626b188.js"
+  },
+  {
+    "revision": "92aa40b0cb91058e93deddcbf40148c8",
+    "url": "/dayplanner/static/media/bell.92aa40b0.mp3"
+  }
+]);
